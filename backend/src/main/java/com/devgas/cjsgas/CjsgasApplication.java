452c2296cb2ea217f65e71e332efdd8c22359530
Michel Silva <micheljose.slv@gmail.com>
2021-09-07 18:28:54 -0300

    project created

---
+++ backend/src/main/java/com/devgas/cjsgas/CjsgasApplication.java
@@ -0,0 +1,13 @@
+package com.devgas.cjsgas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CjsgasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CjsgasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devgas/cjsgas/CjsgasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devgas.cjsgas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CjsgasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
