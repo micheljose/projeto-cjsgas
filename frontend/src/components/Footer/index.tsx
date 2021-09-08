603ba44a912b90b06c1179056bd32f33118606eb
Michel Silva <micheljose.slv@gmail.com>
2021-09-08 18:34:50 -0300

    Basic Static Components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+  <div className="container">
+    <p className="text-light">App desenvolvido por <a href="https://github.com/Michelsilva" target="_blank" rel="noreferrer">Michel Jose da Silva</a></p>
+    <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/michelmga" target="_blank" rel="noreferrer">@michelmga</a></small></p>
+  </div>
+</footer>
+    );
+}
+
+export default Footer;
