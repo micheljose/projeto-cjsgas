bc6f98f8f73a0ce393b0dfac2d806617560b7fb6
Michel Silva <micheljose.slv@gmail.com>
2021-09-11 16:19:04 -0300

    Pagination

---
+++ backend/src/main/java/com/devgas/cjsgas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.devgas.cjsgas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devgas.cjsgas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
