63553c5f8b96a3aedba237eba58d6f455bffb120
Michel Silva <micheljose.slv@gmail.com>
2021-09-14 21:32:55 -0300

    First request

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,12 +1,31 @@
+import axios from "axios";
 import Chart from "react-apexcharts";
+import { SaleSum } from "types/sale";
+import { BASE_URL } from "utils/requests";
+
+type ChartData = {
+    labels: string[];
+    series: number[];
+}
 
 const DonutChart = () => {
+    //FORMA ERRADA
+    let chartData: ChartData = { labels: [], series: [] };
 
+    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+        .then(response => {
+            const data = response.data as SaleSum[];
+            const myLabels = data.map(x => x.sellerName);
+            const mySeries = data.map(x => x.sum);
 
-    const mockData = {
-        series: [477138, 499928, 444867, 320426, 473088],
-        labels: ['Clovis', 'Maria Clara', 'Alessandra', 'Michel', 'Eliane',]
-    }
+            chartData = { labels : myLabels, series: mySeries};
+            console.log(chartData);
+        });
+
+    //const mockData = {
+    //    series: [477138, 499928, 444867, 320426, 473088],
+    //    labels: ['Clovis', 'Maria Clara', 'Alessandra', 'Michel', 'Eliane',]
+    // }
 
     const options = {
         legend: {
@@ -15,8 +34,8 @@ const DonutChart = () => {
     }
     return (
         <Chart
-            options={{ ...options, labels: mockData.labels }}
-            series={mockData.series}
+            options={{ ...options, labels: chartData.labels }}
+            series={chartData.series}
             type="donut"
             height="240"
 
